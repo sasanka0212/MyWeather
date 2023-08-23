1312b311c7f19aeab8cbd2b0dc9d77baa3f37e3a
Sasanka Kundu <sasankak2002@gmail.com>
2023-08-23 21:29:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myweather/ApiInterface.kt
@@ -0,0 +1,14 @@
+package com.example.myweather
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+import retrofit2.Call
+
+interface ApiInterface {
+    @GET("weather")
+    fun getWeatherData(
+        @Query("q") city:String,
+        @Query("appid") appid:String,
+        @Query("units") units:String
+    ) : Call<MyWeather>
+}
